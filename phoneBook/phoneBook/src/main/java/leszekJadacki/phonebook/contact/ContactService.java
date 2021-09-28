b396ac53b11e2378f3fc7a021bfb0615b44c649f
Lesjad <jadacki.leszek@gmail.com>
2021-09-29 01:23:57 +0200

    ContactService.java:  - implemented generic searching for contact

--- phoneBook/phoneBook/src/main/java/leszekJadacki/phonebook/contact/ContactService.java
+++ phoneBook/phoneBook/src/main/java/leszekJadacki/phonebook/contact/ContactService.java
@@ -2,8 +2,10 @@ package leszekJadacki.phonebook.contact;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 
 import javax.persistence.NonUniqueResultException;
 import java.security.Timestamp;
